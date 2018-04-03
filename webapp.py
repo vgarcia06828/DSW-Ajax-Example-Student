7d237b61d28f23b6f5fd599a4318e96fb0ee583f
sky-adams <skyadams@sbunified.org>
2018-04-03 07:55:15 -0700

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, session, request, jsonify, Markup
+from flask import render_template
+
+app = Flask(__name__)
+
+app.debug = True #Change this to False for production
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/update_div')
+def update():
+    return Markup("<p>This text came from the server.</p>")
+
+if __name__ == '__main__':
+    app.run()
